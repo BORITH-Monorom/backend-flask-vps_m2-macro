475ba07ab0256140553d5dcb17ee109e5ca4d5e8
BORITH Monorom <borith.monorom@itlink.com.kh>
2025-08-08 16:19:43 +0700

    update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is my Flask app. hello'
+    return 'Hello, World! This is my Flask app.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
